9415fe3c6588f38fd74ab16abfd12aa5d35a2cbd
Hernan Marano <herchugm@gmail.com>
2013-08-27 15:51:05 -0300

    setting assembly files

---
+++ source/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Hernan Marano")]
+[assembly: AssemblyProduct("Eqip.Globalization")]
+[assembly: AssemblyCopyright("Copyright 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyConfiguration("Release")]
